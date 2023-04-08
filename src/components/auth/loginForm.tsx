66bf1f226babe3e3eeb87b19c3025aa3783a6d48
mightGuy <onyejemeemmanuel65@gmail.com>
2023-04-08 04:03:30 +0100

    Added Form validation

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -1,21 +1,30 @@
 // jshint esversion:6
-import { useRef } from "react";
 import { FacebookSVG, GoogleSVG, LinkedInSVG, EmailSVG, ArrowClockwiseSVG, KeySVG } from "../../assets/svg";
 import { NavLink } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+// Create user Schema for form data
+const schema = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(5, { message: "Password must be 5 or more characters long" })
+})
+
+// Extract inferred type from schema
+type FormData = z.infer<typeof schema>
 
 function LoginForm() {
 
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-    function handleSubmit() {
-        alert("Functionlity coming soon!");
-    }
+    // Submit form
+    const onSubmit = (data: FormData) => console.log(data);
 
     return (
         <>
             <div>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="max-w-[400px] w-full px-4 mx-auto mt-[5rem] flex flex-col items-center">
                         <h1 className="text-pry font-bold text-3xl">Login</h1>
                         <div className="flex justify-center gap-x-5 mt-3">
@@ -26,13 +35,19 @@ function LoginForm() {
 
                         {/* Form fields */}
                         <div className="mt-[4rem] flex flex-col w-full gap-y-5">
-                            <div className="flex h-[3rem] w-full relative">
-                                <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><EmailSVG size={20} /></div>
-                                <input className="rounded w-full h-full pl-[3rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none focus:border-pry" type="email" placeholder="Email" ref={emailRef} />
+                            <div>
+                                <p className="text-sm text-red-700 font-mono">{errors?.email?.message}</p>
+                                <div className="flex h-[3rem] w-full relative">
+                                    <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><EmailSVG size={20} /></div>
+                                    <input className={`rounded w-full h-full pl-[3rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none ${!errors?.email ? "focus:border-pry" : "border-red-700" }`} type="email" placeholder="Email" {...register("email")} />
+                                </div>
                             </div>
-                            <div className="flex h-[3rem] w-full relative">
-                                <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><KeySVG size={20} /></div>
-                                <input className="rounded w-full h-full pl-[3rem] text-[black] bg-gray-200 border-2 text-[1rem] outline-none focus:border-pry" type="password" placeholder="Password" ref={passwordRef} />
+                            <div>
+                                <p className="text-sm text-red-700 font-mono">{errors?.password?.message}</p>
+                                <div className="flex h-[3rem] w-full relative">
+                                    <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><KeySVG size={20} /></div>
+                                    <input className={`rounded w-full h-full pl-[3rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none ${!errors?.password ? "focus:border-pry" : "border-red-700"}`} type="password" placeholder="Password" {...register("password")} />
+                                </div>
                             </div>
                         </div>
                         <div className="w-full flex justify-between">
@@ -45,7 +60,7 @@ function LoginForm() {
                         {/* path to login page*/}
 
                         {/* Form submit buttons */}
-                        <button type="submit" className="self-end px-9 py-3 bg-pry text-[white] rounded mt-8" onClick={handleSubmit}>Login</button>
+                        <button type="submit" className="self-end px-9 py-3 bg-pry text-[white] rounded mt-8">Login</button>
                     </div>
                 </form>
 
--- src/components/auth/signUpForm.tsx
+++ src/components/auth/signUpForm.tsx
@@ -1,22 +1,33 @@
 // jshint esversion:6
-import { useRef } from "react";
 import { FacebookSVG, GoogleSVG, LinkedInSVG, EmailSVG, ArrowClockwiseSVG, KeySVG } from "../../assets/svg";
 import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+
+// Create object schema for form values
+const schema = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(5, { message: "Password must be 5 or more characters long" }),
+    confirmPassword: z.string().min(5, { message: "Password must be 5 or more characters long" })
+})
+    .refine((data) => {
+        return data.password === data.confirmPassword
+    }, { message: "Passwords do not match", path: ["confirmPassword"] });
+
+// Extract the inferred type
+type FormData = z.infer<typeof schema>;
 
 function SignUpForm() {
 
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const confirmPasswordRef = useRef<HTMLInputElement>(null);
+    const onSubmit = (data: FormData) => console.log(data);
 
-    function handleSubmit() {
-        alert("Functionlity coming soon!");
-    }
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
 
     return (
         <>
             <div>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="max-w-[400px] w-full px-4 mx-auto mt-[5rem] flex flex-col items-center">
                         <h1 className="text-pry font-bold text-3xl">Create Account</h1>
                         <div className="flex justify-center gap-x-5 mt-3">
@@ -27,26 +38,36 @@ function SignUpForm() {
 
                         {/* Form fields */}
                         <div className="mt-[4rem] flex flex-col w-full gap-y-5">
-                            <div className="flex h-[3rem] w-full relative">
-                                <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><EmailSVG size={20} /></div>
-                                <input className="rounded w-full h-full pl-[2.5rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none focus:border-pry" type="email" placeholder="Email" ref={emailRef} />
+                            <div>
+                                <p className="text-sm text-red-700 font-mono">{errors?.email?.message}</p>
+                                <div className="flex h-[3rem] w-full relative">
+                                    <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><EmailSVG size={20} /></div>
+                                    <input className={`rounded w-full h-full pl-[3rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none ${!errors?.email ? "focus:border-pry" : "border-red-700"}`} type="email" placeholder="Email" {...register("email")} />
+                                </div>
                             </div>
-                            <div className="flex h-[3rem] w-full relative">
-                                <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><KeySVG size={20} /></div>
-                                <input className="rounded w-full h-full pl-[2.5rem] text-[black] bg-gray-200 border-2 text-[1rem] outline-none focus:border-pry" type="password" placeholder="Password" ref={passwordRef} />
+                            <div>
+                                <p className="text-sm text-red-700 font-mono">{errors?.password?.message}</p>
+                                <div className="flex h-[3rem] w-full relative">
+                                    <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><KeySVG size={20} /></div>
+                                    <input className={`rounded w-full h-full pl-[3rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none ${!errors?.password ? "focus:border-pry" : "border-red-700"}`} type="password" placeholder="Password" {...register("password")} />
+                                </div>
                             </div>
-                            <div className="flex h-[3rem] w-full relative">
-                                <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><ArrowClockwiseSVG size={20} /></div>
-                                <input className="rounded w-full h-full pl-[2.5rem] bg-gray-200 border-2 text-[black] text-[1rem] outline-none focus:border-pry" type="password" placeholder="Confirm Password" ref={confirmPasswordRef} />
+                            <div>
+                                <p className="text-sm text-red-700 font-mono">{errors.confirmPassword?.message}</p>
+                                <div className="flex h-[3rem] w-full relative">
+                                    <div className=" absolute left-[5px] h-full max-w-content flex items-center px-[5px] text-pry"><ArrowClockwiseSVG size={20} /></div>
+                                    <input className={`rounded w-full h-full pl-[3rem] pr-2 text-[black] bg-gray-200 border-2 text-[1rem] outline-none ${!errors?.confirmPassword ? "focus:border-pry" : "border-red-700"}`} type="password" placeholder="Confirm Password" {...register("confirmPassword")} />
+                                </div>
                             </div>
                         </div>
+
                         {/* path to login page*/}
                         <NavLink to={"/auth/login"} className={"self-start"}>
                             <div className="self-start text-gray-600 font-mono mt-2">Login Here</div>
                         </NavLink>
 
                         {/* Form submit buttons */}
-                        <button type="submit" className="self-end px-6 py-3 bg-pry text-[white] rounded mt-8" onClick={handleSubmit}>Sign Up</button>
+                        <button type="submit" className="self-end px-6 py-3 bg-pry text-[white] rounded mt-8">Sign Up</button>
                     </div>
                 </form>
 
